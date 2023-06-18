30b8771b803ef97387de4e276b012a19e8ac78cf
Samal K <safty@mail.ru>
2023-06-18 16:59:37 +0600

    добавлен console.log и рефакторинг функции

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const logged = () => {
     console.log(div.textContent);
 console.log('commits');
 }
+const div = document.getElementById('github')
+
+const logged = () => {
+    console.log(div.textContent);
+console.log('commits');
+}
